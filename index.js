1f196acb93c36f62f408fa05b661d94e4101c34f
Ashley <grigsby.ashley@gmail.com>
2017-10-20 17:59:31 -0400

    Heroku testing

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var express = require(‘express’);
-var app = express();
-
-app.set(‘port’, (process.env.PORT || 8000));
-
-app.use(express.static(__dirname + ‘/’));
-
-app.listen(app.get(‘port’), function() {
-  console.log(“Node app is running at localhost:” + app.get(‘port’));
-});
+var express = require('express')
+var app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
